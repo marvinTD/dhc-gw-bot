5c0ef86faeedac48f5a7ef52f1c332ecd4c1260d
marvinTD <marvin@outlook.com>
2019-02-02 23:00:36 -0600

    Add basic war timer commands

--- bot.js
+++ bot.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 const Discord = require('discord.js');
 const bot = new Discord.Client()
+bot.login(process.env.BOT_SECRET);
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+let warWarning;
+let warEnd;
+
+bot.on('message', (msg) => {
+  if (msg.content === '!war start') {
+    msg.channel.send('@here Guild War has started!');
+    warWarning = setTimeout(() => { msg.channel.send('@here One hour left in war!')}, 5000);
+    warEnd = setTimeout(() => { msg.channel.send('War has ended.') }, 10000);
   }
 });
 
-bot.login(process.env.BOT_SECRET);
+bot.on('message', (msg) => {
+  if (msg.content === '!war end') {
+    msg.channel.send('Current Guild War has been ended.');
+    clearTimeout(warWarning);
+    clearTimeout(warEnd);
+  }
+});
