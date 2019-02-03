af0e9cf59b72892d3d2fe2abe11d4b22007ef63f
marvinTD <marvin@outlook.com>
2019-02-03 00:18:29 -0600

    Add role check and ability to specify war length

--- bot.js
+++ bot.js
@@ -6,18 +6,40 @@ bot.login(process.env.BOT_SECRET);
 let warWarning;
 let warEnd;
 
+function convertToMs(h, m) {
+  return 3600000 * h + 60000 * m;
+}
+
 bot.on('message', (msg) => {
-  if (msg.content === '!war start') {
-    msg.channel.send('@here Guild War has started!');
-    warWarning = setTimeout(() => { msg.channel.send('@here One hour left in war!')}, 5000);
-    warEnd = setTimeout(() => { msg.channel.send('War has ended.') }, 10000);
+  if (msg.member.roles.find((role) => role.name === 'warmonger')) {
+    if (msg.content.startsWith('!war start')) {
+      // currently requires format _h_m
+      time = msg.content.split(' ')[2]
+      let hours = 12;
+      let minutes = 0;
+      let seconds = 0;
+      if (time) {
+        splitTime = time.split(/h|m/);
+        hours = parseInt(splitTime[0]);
+        minutes = parseInt(splitTime[1]);
+      }
+
+      const warDuration = convertToMs(hours, minutes)
+      msg.channel.send(`@here Guild War has started with ${hours} hours and ${minutes} minutes!`);
+      if (warDuration > 3600000) {
+        warWarning = setTimeout(() => { msg.channel.send('@here One hour left in war!') }, warDuration - 3600000);
+      }
+      warEnd = setTimeout(() => { msg.channel.send('War has ended.') }, warDuration);
+    }
   }
 });
 
 bot.on('message', (msg) => {
   if (msg.content === '!war end') {
-    msg.channel.send('Current Guild War has been ended.');
-    clearTimeout(warWarning);
-    clearTimeout(warEnd);
+    if (msg.member.roles.find((role) => role.name === 'warmonger')) {
+      msg.channel.send('Current Guild War has been ended.');
+      clearTimeout(warWarning);
+      clearTimeout(warEnd);
+    }
   }
 });
