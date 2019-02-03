ca30e5028e898d83fdcee7a91caa64f4d39acc66
marvinTD <marvin@outlook.com>
2019-02-03 14:16:11 -0600

    Refactor time logic

--- bot.mjs
+++ bot.mjs
@@ -12,22 +12,14 @@ client.on('message', (msg) => {
   if (msg.member.roles.get(discordIds.adminRole)) {
     if (msg.content.startsWith('!war start')) {
       // currently requires format _h_m
-      const timeStr = msg.content.split(' ')[2];
-      let hours = 12;
-      let minutes = 0;
-      if (timeStr) {
-        const t = timeStr.split(/h|m/);
-        hours = parseInt(t[0], 10);
-        minutes = parseInt(t[1], 10);
+      const warDuration = convertToMs(msg.content.split(' ')[2]);
+      msg.channel.send(`@here Guild War has started with a duration of ${warDuration.text}`);
+      if (warDuration.ms > 3600000) {
+        warWarning = setTimeout(() => { msg.channel.send('@here One hour left in war!'); }, warDuration.ms - 3600000);
       }
-
-      const warDuration = convertToMs(hours, minutes);
-      msg.channel.send(`@here Guild War has started with a duration of ${hours} hours and ${minutes} minutes!`);
-      if (warDuration > 3600000) {
-        warWarning = setTimeout(() => { msg.channel.send('@here One hour left in war!'); }, warDuration - 3600000);
-      }
-      warEnd = setTimeout(() => { msg.channel.send('War has ended.'); }, warDuration);
+      warEnd = setTimeout(() => { msg.channel.send('War has ended.'); }, warDuration.ms);
     }
+
     if (msg.content === '!war end') {
       msg.channel.send('Current Guild War has been ended.');
       clearTimeout(warWarning);
